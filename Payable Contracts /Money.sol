d5d2fee9c597d0751b736552424a0790002ff04f
Ahzam Naseem Kidwai <130897998+ahzamkidwai@users.noreply.github.com>
2025-12-03 12:11:22 +0530

    Implement deposit and withdraw functions in Money contract

--- Payable Contracts /Money.sol
+++ Payable Contracts /Money.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract Money {
+    uint public totalMoney;
+
+    constructor () payable {
+        
+    }
+
+    function deposit() public payable {
+        totalMoney += msg.value;
+    }
+
+    function withdraw(address payable ad) public {
+        require(totalMoney <= address(this).balance, "Contract balance is too low"); 
+        
+        payable(ad).transfer(totalMoney);
+        totalMoney = 0;
+    }
+}
